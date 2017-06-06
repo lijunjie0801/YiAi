85579f6709beb77c888e92ca1229aea8f624777d
zlkj <zlkj@zlkjdeMac-mini.local>
2017-06-06 11:22:05 +0800

    Initial Commit

---
+++ YiAi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YiAi
+//
+//  Created by zlkj on 2017/6/6.
+//  Copyright © 2017年 zlkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ YiAi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YiAi
+//
+//  Created by zlkj on 2017/6/6.
+//  Copyright © 2017年 zlkj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
